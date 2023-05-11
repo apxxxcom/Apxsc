1b97ed255f1dce0b215b1100bc1b90589a259528
apxxx.com <132904732+apxxxcom@users.noreply.github.com>
2023-05-11 19:43:36 +0700

    Add files via upload

---
+++ edit_produk/edit_payment.js
@@ -0,0 +1,23 @@
+const { modul } = require('../module');
+const edit_payment = `Di Bawah`
+
+//EDIT PAYMENT GOPAY DI BAWAH
+const QRgopay = `https://ibb.co/qsf3gpj`
+const NOgopay = `081385643965`
+
+//EDIT PAYMENT DANA DI BAWAH
+const QRdana = `https://ibb.co/qsf3gpj`
+const NOdana = `081385643965`
+
+//EDIT PAYMENT OVO DI BAWAH
+const QRovo = `https://ibb.co/qsf3gpj`
+const NOovo = `081385643965`
+
+//EDIT PAYMENT SHOPEE DI BAWAH
+const QRshopee = `https://ibb.co/qsf3gpj`
+const NOshopee = `081385643965`
+
+//EDIT ALL PAYMENT DI BAWAH
+const QRallpay = `https://d.top4top.io/p_265331x8e2.jpg`
+
+module.exports = { edit_payment,QRgopay,NOgopay,QRdana,NOdana,QRovo,NOovo,QRshopee,NOshopee,QRallpay }
